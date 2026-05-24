396912aeeabea4a1c7f82b9939616938401d6fc5
jonusadev <jonusadeveloper@gmail.com>
2026-05-24 09:43:14 +0700

    ea

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * Set to '*' to trust all proxies. For tighter security, set the proxy IP(s)
+     * via the TRUSTED_PROXIES environment variable.
+     *
+     * @var array<int, string>|string|null
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_FOR
+        | Request::HEADER_X_FORWARDED_HOST
+        | Request::HEADER_X_FORWARDED_PORT
+        | Request::HEADER_X_FORWARDED_PROTO;
+}

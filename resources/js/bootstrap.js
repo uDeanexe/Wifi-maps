0c3245aa5326b633c4b59f3c6e1643fdd32544d3
udean <97400931+uDeanexe@users.noreply.github.com>
2026-06-27 20:21:18 +0700

    Load flash toast handler

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import './map-cable-enhancements';
+import './flash-toasts';
 
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
